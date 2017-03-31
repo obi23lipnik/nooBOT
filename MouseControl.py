e4bc326a4df8e9eaad5fdf9957bd7ab225e1e125
obi <obi_lipnik@yahoo.co.uk>
2017-03-31 22:31:34 +0200

    <insert dank joke about losing hope>

--- MouseControl.py
+++ MouseControl.py
@@ -1,106 +1,112 @@
-import win32api, win32con
 from time import sleep
 from random import randint
 
+import win32api, win32con
+
+
 class Action(object):
-    # Init will require input of absolute coordinates (by pixle) for each angle point
-    ## If no values are specified, the surface is set to 800x600 and with minimum coordinate of (0, 0) -
+    # Init will require input of absolute coordinates (by pixel) for each angle point
+    ## If no values are specified, the surface is set to 800x600 and with minimum coordinate of (0, 0)
     def __init__(self, x_minimum=0, y_minimum=0, x_maximum=800, y_maximum=600):
-        self.x_minimum = x_minimum
-        self.y_minimum = y_minimum
+        self.x_lowest = x_minimum
+        self.y_lowest = y_minimum
         self.x_maximum = x_maximum
         self.y_maximum = y_maximum
 
-    def get_pos(self):
+    @staticmethod
+    def is_reversed(value):
+        return value < 0
+
+    @staticmethod
+    def get_pos():
         return win32api.GetCursorPos()
 
     def set_pos(self, x, y, verbose=False):
-        x, y = self.x_minimum + x, self.y_minimum + y
-
         if x > self.x_maximum:
             x = self.x_maximum
+        elif x < self.x_lowest:
+            x = self.x_lowest
 
         if y > self.y_maximum:
             y = self.y_maximum
+        elif y < self.y_lowest:
+            y = self.y_lowest
 
         win32api.SetCursorPos((x, y))
 
         if verbose:
-            return 'Moved to ({0}, {1})'.format(x, y)
+            print 'Moved to ({0}, {1})'.format(x, y)
 
-    def pseudo_human_move(self, xm=0, ym=0):
-        reverse_xm, reverse_ym = False, False
-        xm, ym = int(xm), int(ym)
+    @staticmethod
+    def bow_binary_range(x_len, y_len):
+        if x_len >= y_len:
+            division = x_len / y_len
+            point = x_len % y_len
+            if point == 0:
+                dir_range = [True for direction in range(x_len)]
+                i = 0
+                for direction in dir_range:
+                    if
 
-        if xm < 0:
-            reverse_xm = True
-            xm *= -1
 
-        if ym < 0:
-            reverse_ym = True
-            ym *= -1
 
-        for _ in range(ym):
+
+    def pseudo_human_move(self, move_right=0, move_down=0, verbose=False):
+        reverse_xm, reverse_ym = self.is_reversed(move_right), self.is_reversed(move_down)
+        move_right, move_down = int(move_right), int(move_down)
+
+        for _ in range(abs(move_down)):
             sleep(0.001)
             x, y = self.get_pos()
             if reverse_ym:
-                self.set_pos(x, y-1)
+                self.set_pos(x, y - 1, verbose)
             else:
-                self.set_pos(x, y+1)
+                self.set_pos(x, y + 1, verbose)
 
-        for _ in range(xm):
-            sleep(0.001)
-            x, y = self.get_pos()
-            if reverse_xm:
-                self.set_pos(x-1, y)
-            else:
-                self.set_pos(x+1, y)
+    def pseudo_human_sleep(self, ):
+        sleep(float('{sec}.{msec}'.format(sec=randint(0, 1), msec=randint(100, 999))))
 
-    def human_sleep(self, ):
-        sleep(float('{sec}.{msec}'.format(sec=randint(0, 1), msec=randint(100,999))))
-
-    def wait(self, sec, dsec, msec):
+    def wait(self, sec=0, dsec=0, msec=0):
         sleep(float('{sec}.{dsec}{msec}'.format(sec=sec, dsec=dsec, msec=msec)))
 
     def right_click(self, x=None, y=None):
         if not x or not y:
             x, y = self.get_pos()
-        self.set_pos(x,y)
-        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,x,y,0,0)
-        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,x,y,0,0)
+        self.set_pos(x, y)
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, x, y, 0, 0)
 
     def right_hold(self, x=None, y=None):
         if not x or not y:
             x, y = self.get_pos()
 
-        self.set_pos(x,y)
-        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,x,y,0,0)
+        self.set_pos(x, y)
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0)
 
     def right_release(self, x=None, y=None):
         if not x or not y:
             x, y = self.get_pos()
 
-        self.set_pos(x,y)
-        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,x,y,0,0)
+        self.set_pos(x, y)
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, x, y, 0, 0)
 
     def left_click(self, x=None, y=None):
         if not x or not y:
             x, y = self.get_pos()
-        self.set_pos(x,y)
-        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+        self.set_pos(x, y)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 
     def left_hold(self, x=None, y=None):
         if not x or not y:
             x, y = self.get_pos()
 
-        self.set_pos(x,y)
-        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+        self.set_pos(x, y)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
 
     def left_release(self, x=None, y=None):
         if not x or not y:
             x, y = self.get_pos()
 
-        self.set_pos(x,y)
-        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
-
+        self.set_pos(x, y)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
--- script.py
+++ script.py
@@ -1,6 +1,5 @@
-import MouseControl as bot
-bot.wait(1,0,0)
-bot.right_hold()
-bot.wait(0,0,40)
-bot.relative_move(600,0)
-bot.right_release()
+from MouseControl import Action as Mouse_Action
+
+mouse = Mouse_Action(x_minimum=62, y_minimum=0, x_maximum=862, y_maximum=600)
+
+print mouse.bow_binary_range(range(3),range(10))
